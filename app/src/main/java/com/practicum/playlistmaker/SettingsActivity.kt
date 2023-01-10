3bff5c1165f314b4e07ef4f2ad7b99705d81a9e0
Margarita Bespalova <margo.ivi@yandex.ru>
2023-01-10 22:15:35 +0300

    9 Sprint: Second Homework

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,48 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val share = findViewById<FrameLayout>(R.id.share)
+        val support = findViewById<FrameLayout>(R.id.support)
+        val termsOfUse = findViewById<FrameLayout>(R.id.terms_of_use)
+
+        share.setOnClickListener {
+            val message = "https://practicum.yandex.ru/android-developer/"
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "Поделиться"))
+        }
+
+        support.setOnClickListener {
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val title = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("margo.ivi@yandex.ru"))
+            intent.putExtra(Intent.EXTRA_SUBJECT, title)
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(intent)
+
+        }
+
+        termsOfUse.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            startActivity(intent)
+        }
+
+
+
+
     }
 }
