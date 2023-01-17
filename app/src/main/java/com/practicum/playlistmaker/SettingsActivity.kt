56a3326233e628c87cfc97daab98eccb36067f29
Margarita Bespalova <margo.ivi@yandex.ru>
2023-01-17 20:30:09 +0300

    9 sprint after review

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -23,25 +23,28 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         share.setOnClickListener {
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_url))
-            intent.type = "text/plain"
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.share_url))
+                type = "text/plain"
+            }
             startActivity(Intent.createChooser(intent, getString(R.string.chooser_title)))
         }
 
         support.setOnClickListener {
-            val intent = Intent(Intent.ACTION_SENDTO)
-            intent.data = Uri.parse("mailto:")
-            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("margo.ivi@yandex.ru"))
-            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_title))
-            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.support_message))
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(R.string.email))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_title))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.support_message))
+            }
 
             startActivity(Intent.createChooser(intent, getString(R.string.chooser_title)))
         }
 
         termsOfUse.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse(getString(R.string.offer_url))
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(getString(R.string.offer_url))
+            }
             startActivity(intent)
         }
 
