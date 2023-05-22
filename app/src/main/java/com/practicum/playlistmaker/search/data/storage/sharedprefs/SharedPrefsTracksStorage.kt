f132a07da0da355b7810f74f797966520eda3d90
Margarita Bespalova <margo.ivi@yandex.ru>
2023-05-22 10:37:18 +0300

    test commit from ssh key

--- app/src/main/java/com/practicum/playlistmaker/search/data/storage/sharedprefs/SharedPrefsTracksStorage.kt
+++ app/src/main/java/com/practicum/playlistmaker/search/data/storage/sharedprefs/SharedPrefsTracksStorage.kt
@@ -1,12 +1,14 @@
 package com.practicum.playlistmaker.search.data.storage.sharedprefs
 
 import android.content.SharedPreferences
+import android.util.Log
 import com.google.gson.Gson
 import com.practicum.playlistmaker.search.data.storage.models.TrackModelDto
 
 class SharedPrefsTracksStorage(private val sharedPreferences: SharedPreferences) : ITracksStorage {
     
     override fun saveHistory(historyList: List<TrackModelDto>) {
+        Log.d("STORAGE", "+++save+++")
         val json = Gson().toJson(historyList)
         
         sharedPreferences
@@ -16,6 +18,7 @@ class SharedPrefsTracksStorage(private val sharedPreferences: SharedPreferences)
     }
     
     override fun readHistory(): Array<TrackModelDto> {
+        Log.d("STORAGE", "+++read+++")
         
         val json = sharedPreferences.getString(HISTORY_LIST_KEY, null) ?: return emptyArray()
         return Gson().fromJson(json, Array<TrackModelDto>::class.java)
