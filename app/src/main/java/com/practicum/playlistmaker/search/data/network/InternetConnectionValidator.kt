c3acccba7c313721de6da9c0f3d930afda79e4db
Margarita Starkova <margaritabespa@gmail.com>
2024-03-10 11:38:18 +0300

    test xommit

--- app/src/main/java/com/practicum/playlistmaker/search/data/network/InternetConnectionValidator.kt
+++ app/src/main/java/com/practicum/playlistmaker/search/data/network/InternetConnectionValidator.kt
@@ -10,6 +10,8 @@ class InternetConnectionValidator(private val context: Context) {
         val connectivityManager = context.getSystemService(
             Context.CONNECTIVITY_SERVICE
         ) as ConnectivityManager
+
+        //тестовый коммит
         
         val capabilities =
             connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
